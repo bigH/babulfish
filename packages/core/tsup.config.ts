d616dfcd5d1fe24ddef1af0acff3ceb0ba02bb7c
Hiren Hiranandani <hiren.hiranandani@chime.com>
2026-04-14 12:57:46 -0700

    core: clean dist before publish

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from "tsup"
 
-export default defineConfig([
-  {
-    entry: {
-      index: "src/index.ts",
-      engine: "src/engine/index.ts",
-      dom: "src/dom/index.ts",
-      "engine/testing": "src/engine/testing/index.ts",
-      testing: "src/testing/index.ts",
-    },
-    format: ["esm"],
-    dts: true,
-    sourcemap: true,
-    external: ["@huggingface/transformers"],
-    treeshake: true,
-    outDir: "dist",
+export default defineConfig({
+  entry: {
+    index: "src/index.ts",
+    engine: "src/engine/index.ts",
+    dom: "src/dom/index.ts",
+    "engine/testing": "src/engine/testing/index.ts",
+    testing: "src/testing/index.ts",
   },
-])
+  clean: true,
+  format: ["esm"],
+  dts: true,
+  sourcemap: true,
+  external: ["@huggingface/transformers"],
+  treeshake: true,
+  outDir: "dist",
+})
