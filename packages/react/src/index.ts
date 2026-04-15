1b4b18b0ddfc209e7229b7f1a22a423b3c7e4ca0
Hiren Hiranandani <hiren.hiranandani@chime.com>
2026-04-15 09:14:39 -0700

    refactor: simplify react public api exports

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,26 +1,12 @@
-export {
-  TranslatorProvider,
-  type TranslatorConfig,
-  type TranslatorLanguage,
-} from "./provider.js"
+export { TranslatorProvider } from "./provider.js"
+export { useTranslator } from "./use-translator.js"
+export { useTranslateDOM } from "./use-translate-dom.js"
+export { TranslateButton } from "./translate-button.js"
+export { TranslateDropdown } from "./translate-dropdown.js"
 
 export { DEFAULT_LANGUAGES } from "@babulfish/core"
 
-export {
-  useTranslator,
-  type ModelState,
-  type TranslationState,
-} from "./use-translator.js"
-
-export { useTranslateDOM } from "./use-translate-dom.js"
-
-export {
-  TranslateButton,
-  type TranslateButtonClassNames,
-  type TranslateButtonProps,
-} from "./translate-button.js"
-
-export {
-  TranslateDropdown,
-  type TranslateDropdownProps,
-} from "./translate-dropdown.js"
+export type { ModelState, TranslationState } from "@babulfish/core"
+export type { TranslatorConfig, TranslatorLanguage } from "./provider.js"
+export type { TranslateButtonClassNames, TranslateButtonProps } from "./translate-button.js"
+export type { TranslateDropdownProps } from "./translate-dropdown.js"
