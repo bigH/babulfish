8bac5be5e93e2c1ab34c3321030d07da710dc0b3
Hiren Hiranandani <hiren.hiranandani@chime.com>
2026-04-15 10:01:31 -0700

    refactor(react): simplify tsup config

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from "tsup"
 
-export default defineConfig([
-  {
-    entry: {
-      index: "src/index.ts",
-    },
-    format: ["esm"],
-    dts: true,
-    sourcemap: true,
-    external: ["react", "react-dom", "@babulfish/core", "@babulfish/styles"],
-    treeshake: true,
-    outDir: "dist",
+export default defineConfig({
+  entry: {
+    index: "src/index.ts",
   },
-])
+  format: "esm",
+  dts: true,
+  sourcemap: true,
+  external: ["react", "@babulfish/core", "@babulfish/styles"],
+  treeshake: true,
+  outDir: "dist",
+})
